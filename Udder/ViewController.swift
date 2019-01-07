496274ffb4ae343872e643f7e26be66bb981b39a
BuildTools <unconfigured@null.spigotmc.org>
2019-01-07 12:36:37 -0500

    Add view did disappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -19,5 +19,9 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         //
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        //
+    }
 }
 
